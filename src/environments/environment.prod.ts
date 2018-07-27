ea64e432a3203d88bf4296299c058c54676e5277
cheq326 <your-email-address>
2018-07-26 21:09:41 -0400

    added missing '/' in swarm api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   // web3HttpProvider: "http://172.31.83.105:8501",
   OothAPI: 'http://linkcryptocoin.com:8091/auth/',
   MongoAPI: 'http://linkcryptocoin.com:8080/api/',
-  SwarmProvider: 'https://linkgear.net:5000',
+  SwarmProvider: 'https://linkgear.net:5000/',
   inactivitySec: 300,
   pingIntervalSec: 15,
   chainPageImageMaxSize: 2000000,
