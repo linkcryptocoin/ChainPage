656a7caa3f9a4a2db6d1e0c07a9f7d319b00ca7c
cheq326 <your-email-address>
2018-07-28 22:33:38 -0400

    change qa envir settings to prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,27 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  HttpProvider: "http://localhost:8545",
-  // web3HttpProvider: "http://34.238.58.243:8506",
-  OothAPI: 'http://23.238.58.243:8091/auth/',
-  MongoAPI: 'http://23.238.58.243:8080/api/',
-  SwarmProvider: 'http://34.238.58.243:5001/',
-  // SwarmProvider: 'http://swarm-gateways.net/',
+  //Prod configuration
+  production: true,
+  HttpProvider: "",
+  // web3HttpProvider: "http://172.31.83.105:8501",
+  OothAPI: 'https://linkgear.net:8091/auth/',
+  MongoAPI: 'https://linkgear.net:8080/api/',
+  SwarmProvider: 'http://linkgear.net:5000/',
   inactivitySec: 300,
   pingIntervalSec: 15,
   chainPageImageMaxSize: 2000000,
   chainPageImageMaxCount: 1
+  //QA configuration
+  // production: false,
+  // HttpProvider: "http://localhost:8545",
+  // // web3HttpProvider: "http://34.238.58.243:8506",
+  // OothAPI: 'http://23.238.58.243:8091/auth/',
+  // MongoAPI: 'http://23.238.58.243:8080/api/',
+  // SwarmProvider: 'http://34.238.58.243:5001/',
+  // // SwarmProvider: 'http://swarm-gateways.net/',
+  // inactivitySec: 300,
+  // pingIntervalSec: 15,
+  // chainPageImageMaxSize: 2000000,
+  // chainPageImageMaxCount: 1
 };
