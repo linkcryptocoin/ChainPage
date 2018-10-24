f90d0e1c641437c2d6c97a634289e8845bfb560f
Dennis Chen <cheq326@msn.com>
2018-10-24 16:21:52 -0400

    updated activation

--- src/app/activation/activation.component.ts
+++ src/app/activation/activation.component.ts
@@ -11,13 +11,17 @@ export class ActivationComponent implements OnInit {
 
   private userName: string;
   private action: string;
+  private sucess: boolean;
   constructor(private route: ActivatedRoute, private oothService: OothService) { 
     this.route.queryParams.subscribe(params => {
       // console.log(params['id']);
+      this.sucess = false;
       this.userName = params['user'];
       this.action = params['action'];
       console.log(this.userName, this.action);
-      oothService.onActivateUser(this.userName, this.action);
+      oothService.onActivateUser(this.userName, this.action)
+      .then(() => { this.sucess = true;})
+      .catch(() => {this.sucess = false;});
     });
   }
 
